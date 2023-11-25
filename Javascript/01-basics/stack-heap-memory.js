ef715dd3152cc7925c0b1bbd03f7b98ef8f6ca26
negisandeep <sandeep16dec@gmail.com>
2023-11-25 13:57:51 +0000

    stack-heap

---
+++ Javascript/01-basics/stack-heap-memory.js
@@ -0,0 +1,14 @@
+// Stack Memory
+// Primitive Types
+// We get Copy of variable
+
+let myYouTubeName = 'youtuber.com';
+let anotherName = myYouTubeName;
+anotherName = "chai aur code";
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+// Heap Memory
+//  Non Primitive types
+// Works on Reference
