1d51a650cf25186e6573c4eddf9b93d27113df00
negisandeep <sandeep16dec@gmail.com>
2023-12-06 11:30:32 +0000

    Updated Changes

--- Projects/FoodOrder/app.js
+++ Projects/FoodOrder/app.js
@@ -3,8 +3,14 @@ const addRemoveItems = document.querySelectorAll('.item-counter');
 const overlay = document.querySelector(".overlay");
 const totalAmount = document.querySelector(".total-order-amount");
 
+// Order Summary Section
+const addedOrderInfo = document.querySelector('.added-order-container');
+const addedOrderItemCount = document.querySelector('.added-items');
+const addedItemTotalPrice = document.querySelector('.added-items-price');
+
+
 const orderList = [];
-let allOrderItems = [];
+let currentUpdatedOrders = [];
 let totalOrderCount;
 let billAmount;
 
@@ -25,132 +31,108 @@ addItems.forEach((item) => {
   item.addEventListener('click', (e) => {
     item.classList.add('d-none');
     const nextSibElement = item.nextElementSibling;
+
+    const itemName = nextSibElement.dataset.name;
+    const itemPrice = nextSibElement.dataset.price;
+    const orderCountNumber = nextSibElement.querySelector('.itemCount');
+
+    let currentItemCount;
+
     nextSibElement.classList.remove('d-none');
-    addOrRemoveItem(nextSibElement);
-  });
-});
-
-function addOrRemoveItem(nextSibElement) {
-
-  const itemName = nextSibElement.dataset.name;
-  const itemPrice = nextSibElement.dataset.price;
-  const removeOrder = nextSibElement.querySelector('.minus');
-  const addOrder = nextSibElement.querySelector('.add');
-  const orderCountNumber = nextSibElement.querySelector('.itemCount');
-
-  // Order Summary Info Display
-  const addedOrderInfo = document.querySelector('.added-order-container');
-  const addedOrderItemCount = document.querySelector('.added-items');
-  const addedItemTotalPrice = document.querySelector('.added-items-price');
 
 
-  let currentItemCount = parseInt(orderCountNumber.innerText);
-
-  const orderInfo = {
-    "name": itemName,
-    "price": parseInt(itemPrice),
-    "count": currentItemCount,
-  }
-
-  removeOrder.addEventListener('click', (e) => {
-    console.log("=========== Remove Section ===========")
-
-    if (currentItemCount === 1) {
-      currentItemCount -= 1;
-      nextSibElement.classList.add('d-none');
-      nextSibElement.previousElementSibling.classList.remove('d-none');
-      console.log(itemName, itemPrice, currentItemCount);
-
-      if (totalOrderCount.length === 0) {
-        addedOrderInfo.classList.add('d-none');
-      }
-
+    const orderInfo = {
+      "name": itemName,
+      "price": parseInt(itemPrice),
+      "count": 1,
     }
-    else if (currentItemCount > 1) {
-      currentItemCount -= 1;
-      orderCountNumber.innerText = currentItemCount;
-      console.log("Minus > 1 => current value = ", parseInt(orderCountNumber.innerText));
-      console.log(itemName, itemPrice, currentItemCount);
-    }
-    orderInfo.count = currentItemCount;
+
     orderList.push(orderInfo);
-
-    allOrderItems = updateOrderList(orderList, orderInfo);
-    console.log("Total Order Summary =", allOrderItems);
-
-    totalOrderCount = getTotalAddedOrdersCount(allOrderItems);
-    addedOrderItemCount.innerText = totalOrderCount + " Item(s)";
-    billAmount = getTotalBillAmount(allOrderItems);
-    addedItemTotalPrice.innerText = "₹" + billAmount;
-
-    e.stopImmediatePropagation();
-
-  });
-
-  addOrder.addEventListener('click', (e) => {
     addedOrderInfo.classList.remove('d-none');
 
-    currentItemCount += 1;
-    orderCountNumber.innerText = currentItemCount;
-    orderInfo.count = currentItemCount;
-
-    orderList.push(orderInfo);
-
-    allOrderItems = updateOrderList(orderList, orderInfo);
-    console.log("Total Order Summary =", allOrderItems);
-
-    totalOrderCount = getTotalAddedOrdersCount(allOrderItems);
+    totalOrderCount = getTotalAddedOrdersCount(orderList);
     addedOrderItemCount.innerText = totalOrderCount + " Item(s)";
-    billAmount = getTotalBillAmount(allOrderItems);
+    billAmount = getTotalBillAmount(orderList);
     addedItemTotalPrice.innerText = "₹" + billAmount;
 
+    currentUpdatedOrders = updatedOrderList(orderList);
 
-    e.stopImmediatePropagation();
+    nextSibElement.addEventListener('click', (e) => {
+
+      if (e.target.className === 'minus') {
+
+        currentItemCount = parseInt(orderCountNumber.innerText);
+
+        if (currentItemCount === 1) {
+          currentItemCount -= 1;
+          nextSibElement.classList.add('d-none');
+          nextSibElement.previousElementSibling.classList.remove('d-none');
+
+          orderInfo.count = currentItemCount;
+
+          currentUpdatedOrders = updatedOrderList(orderList);
+
+          if (currentUpdatedOrders.length === 0) {
+            addedOrderInfo.classList.add('d-none');
+          }
+        }
+        else if (currentItemCount > 1) {
+          currentItemCount -= 1;
+          orderCountNumber.innerText = currentItemCount;
+
+          let orderStatus = isOrderAlreadyExists(orderInfo);
+
+          if (orderStatus.length != 0) {
+            orderInfo.count = currentItemCount;
+          }
+          currentUpdatedOrders = updatedOrderList(orderList);
+        }
+
+        totalOrderCount = getTotalAddedOrdersCount(orderList);
+        addedOrderItemCount.innerText = totalOrderCount + " Item(s)";
+        billAmount = getTotalBillAmount(orderList);
+        addedItemTotalPrice.innerText = "₹" + billAmount;
+
+        e.stopImmediatePropagation();
+      }
+      else if (e.target.className === 'add') {
+
+        currentItemCount = parseInt(orderCountNumber.innerText);
+        currentItemCount += 1;
+        orderCountNumber.innerText = currentItemCount;
+
+        let orderStatus = isOrderAlreadyExists(orderInfo);
+
+        if (orderStatus.length != 0) {
+          orderInfo.count = currentItemCount;
+        } else {
+          orderList.push(orderInfo);
+        }
+
+        currentUpdatedOrders = updatedOrderList(orderList);
+
+        totalOrderCount = getTotalAddedOrdersCount(orderList);
+        addedOrderItemCount.innerText = totalOrderCount + " Item(s)";
+        billAmount = getTotalBillAmount(orderList);
+        addedItemTotalPrice.innerText = "₹" + billAmount;
+
+        e.stopImmediatePropagation();
+      }
+    })
   });
-
-  function updateOrderList(orders, info) {
-    console.log(" <= ors => ", orders);
-    console.log("<= inf => ", info);
-
-    const pr = Array.from(new Set(orders));
-    console.log("all unique value = ", pr);
-    return pr;
-  }
-
-  function getTotalBillAmount(allOrderItems) {
-    const orderItemsPrice = allOrderItems.reduce((sum, items) => {
-      return (sum + items.price * items.count);
-    }, 0);
-
-    console.log("Total Amount = ", orderItemsPrice);
-    return orderItemsPrice;
-  };
-}
-
-function getTotalAddedOrdersCount(allOrderItems) {
-  const totalOrders = allOrderItems.filter((item) => {
-    return item.count != 0;
-  });
-  console.log("total Count = ", totalOrders);
-  return totalOrders.length;
-}
+});
 
 const cart = document.querySelector('#cart-icon');
 const billInfoSummary = document.querySelector('.bill-info');
 
 cart.addEventListener('click', (e) => {
 
-  console.log('Clicked cart');
-  console.log('Cart Items', allOrderItems);
-  console.log('Total Price', billAmount);
-  console.log('Item count', totalOrderCount);
-
   const openModal = document.querySelector('.view-bill');
   const orderInfo = document.querySelector('.added-order-container');
   openModal.classList.remove('d-none');
   overlay.classList.remove("d-none");
 
-  allOrderItems.forEach((item) => {
+  currentUpdatedOrders.forEach((item) => {
     let mainDiv = document.createElement('div');
     let itemName = document.createElement('h1');
     let itemPrice = document.createElement('h1');
@@ -170,7 +152,6 @@ cart.addEventListener('click', (e) => {
       itemCount.innerText = item.count;
       itemTotalPrice.innerText = (item.price * item.count)
     }
-
     totalAmount.innerText = "₹" + billAmount;
   });
   orderInfo.classList.add('d-none');
@@ -187,8 +168,37 @@ closeModal.addEventListener('click', () => {
 
   const existingItems = document.querySelector('.bill-info');
   while (existingItems.hasChildNodes()) {
-    console.log("Child First", existingItems.firstChild)
     existingItems.removeChild(existingItems.firstChild);
   }
-
 });
+
+// Returns Whether order already exists or not
+function isOrderAlreadyExists(orderInfo) {
+  let orderItemsInList = orderList.filter((item) => {
+    return item.name === orderInfo.name;
+  });
+  return orderItemsInList;
+
+}
+function updatedOrderList(orderList) {
+  const arry = orderList.filter((order) => {
+    return order.count != 0;
+  });
+  return arry;
+}
+
+function getTotalBillAmount(allOrderItems) {
+  const orderItemsPrice = allOrderItems.reduce((sum, items) => {
+    return (sum + items.price * items.count);
+  }, 0);
+  return orderItemsPrice;
+};
+
+
+function getTotalAddedOrdersCount(allOrderItems) {
+  const totalOrders = allOrderItems.filter((item) => {
+    return item.count != 0;
+  });
+  return totalOrders.length;
+}
+
