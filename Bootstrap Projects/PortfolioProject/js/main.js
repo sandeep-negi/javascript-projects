82cee5befd4ca59bb7f2a318714b843dc4a8a8ae
negisandeep <sandeep16dec@gmail.com>
2024-01-24 16:20:17 +0000

    Portfolio changes

--- Bootstrap Projects/PortfolioProject/js/main.js
+++ Bootstrap Projects/PortfolioProject/js/main.js
@@ -1,18 +1,18 @@
-/* Header Scroll */
-let nav = document.querySelector('.navbar');
+// Header Scroll
+let nav = document.querySelector(".navbar");
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
-    nav.classList.add('header-scrolled');
+    nav.classList.add("header-scrolled");
   } else {
-    nav.classList.remove('header-scrolled');
+    nav.classList.remove("header-scrolled");
   }
 }
 
-/* Nav Hide in mobile view */
-let navbar = document.querySelectorAll('.nav-link');
-let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navbar.forEach((x) => {
-  x.addEventListener('click', () => {
-    navCollapse.classList.remove('show');
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
   })
 })
